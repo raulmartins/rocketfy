0d275521c7b8fa86848df921ab262816ff9d3fed
Raul Lima <raul.lima@globalhitss.com.br>
2019-08-06 00:44:04 -0300

    initial commit with global style

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import GlobalStyle from './styles/global'
+
+export default function App() {
+  return (
+    <GlobalStyle>
+      <div className="App" />
+    </GlobalStyle>
+  )
+}
